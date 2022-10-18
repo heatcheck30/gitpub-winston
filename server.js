47e7c82497a16ab3c7a249250173c8f47a8b8d0e
Winston Tang <winstontang@Winstons-MBP.attlocal.net>
2022-10-18 12:18:48 -0700

    feat: /drinks route created

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const drinks = require('./models/drinks')
+
+
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Gitpub App!')
 })
+
+app.get('/drinks', (req, res) => {
+    res.send(drinks)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 app.listen(port, () => {
