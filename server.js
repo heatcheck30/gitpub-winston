615fa4b9d986a0c232809f5a4a0523cbf9bc014b
Winston Tang <winstontang@Winstons-MBP.attlocal.net>
2022-10-18 23:13:50 -0700

    feat: final

--- server.js
+++ server.js
@@ -2,34 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 const drinks = require('./models/drinks')
+const food = require('./models/food')
 
-
-
-app.get('/', (req, res) => {
-    res.send('Welcome to the Gitpub App!')
+app.get('/drinks/', (req,res) => {
+    res.render( 'drinks_index.ejs', {allDrinks: drinks, allFood: food})
 })
 
-app.get('/drinks/:whatever', (req, res) => {
-    res.send(req.params.whatever)
+app.get('/drinks/:id', (req, res) => {
+    res.render('drinks_show.ejs', {drinks: drinks[req.params.id]})  
+}) 
+
+
+app.get('/food/:id', (req, res) => {
+    res.render('food_show.ejs', {food: food[req.params.id]})  
 })
-
-
-app.get('/drinks', (req, res) => {
-    res.render('drinks_index.ejs', { allDrinks: drinks });
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -37,5 +23,5 @@ app.get('/drinks', (req, res) => {
 
 
 app.listen(port, () => {
-    console.log('listening!');
-});
+    console.log('listening')
+})
